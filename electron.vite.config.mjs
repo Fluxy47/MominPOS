f94032dd1c711cb5520c420519c36e1c611a60cb
Fluxy47 <ali.scorpio47@gmail.com>
2025-09-03 11:33:21 +0500

    secret changes

--- electron.vite.config.mjs
+++ electron.vite.config.mjs
@@ -2,6 +2,8 @@ import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 
+const isDev = process.env.NODE_ENV !== 'production'
+
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()]
@@ -15,6 +17,14 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react()],
+    server: {
+      headers: {
+        // 👇 loosen CSP in dev so HMR + react-refresh works
+        'Content-Security-Policy': isDev
+          ? "default-src * 'unsafe-inline' 'unsafe-eval'; style-src * 'unsafe-inline'; img-src * data: blob:; connect-src *;"
+          : "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' data:; connect-src 'self';"
+      }
+    }
   }
 })
